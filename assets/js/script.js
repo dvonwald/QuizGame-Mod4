f69d2cd690660f2b8177803e2d797dafb649b784
Dylan <dylan.vonwald@gmail.com>
2022-05-30 16:06:44 -0600

    Got the append to li from localstorage working

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,7 @@ var endGameScreen = $('#end-game-screen')
 var scoreText = $('#score-text')
 var initials = $('#initials-input')
 var scoreSubmit = $('#score-submit')
+var highScoresList = $('#high-scores-list')
 
 //Question & Answer Object Array
 var quizArray = [
@@ -75,13 +76,11 @@ var quizArray = [
 // Game Variables
 var timer = 30;
 currentIndex = 0;
-// console.log(quizArray[currentIndex].question)
-// console.log(quizArray[5])
 var score = 0
-var highScoresList = []
+// var highScoresList = []
 // var inputInitials = ""
 
-console.log(quizArray.length)
+// console.log(quizArray.length)
 
 
 //Page when user first loads
@@ -137,40 +136,45 @@ function renderQuiz() {
 // Penalty Function
 
 // Store High Scores in Local Storage
-// var currentScores = JSON.parse(localStorage.getItem())
-// var currentScoreLi = $(initials).on('submit', ).val();
-
 scoreSubmit.on('click', highScoreSubmit);
 
 function highScoreSubmit() {
     inputInitials = $(initials).on('click').val(); // capture user submitted initials
     $('input[type="text"]').val(""); // clear out form after submit
-    var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-    var highScoreInput = {
+    var highScoresArray = JSON.parse(localStorage.getItem('highScores')) || []; // delcaring the array for localstorage
+    var highScoreObj = {  // declaring the object variable for player input initials and player score
         player: inputInitials,
         playerScore: score,
     }
-    highScores.push(highScoreInput)
-    localStorage.setItem('player', JSON.stringify(highScores))
-    // var highScoreArray = (currentScoreLi + '--' + score)
-    // highScoresList.push(highScoreArray)
-//     console.log(highScoresList)
-//     window.localStorage.setItem('player', highScoreString)
+    highScoresArray.push(highScoreObj)  // pushing the player object to the array
+    localStorage.setItem('highScores', JSON.stringify(highScoresArray)) // stringifying the array for localstorage
+    showScores();
+    // console.log(highScoresArray)
+    // console.log(highScoreObj)
+    // console.log(typeof localStorage.highScores)
+    appendScores();
 }
 
 // Append High Scores as List Items 
+function appendScores() {
+    var highScoresArray = JSON.parse(localStorage.getItem('highScores')) || [];
+    console.log(highScoresArray)
+    for (var i = 0; i < highScoresArray.length; i++) {
+        var scoreObj = highScoresArray[i];
+        var newLi = $("<li>", {
+            class: "list-group-item"
+        })
+        newLi.text(scoreObj.player + "--" + scoreObj.playerScore)
+        highScoresList.append(newLi)
+    }
+}
+
 
 // End Game
 function endGame() {
     gameSec.hide()
     endGameScreen.show()
     scoreText.text("Your final score is: " + score)
-
-    // inputInitials = $(initials).on('click').val();
-    // $('input[type="text"]').val("");
-    // highScoresList.push(highScoreInput)
-    // localStorage.setItem('highScore', JSON.stringify(highScoresList))
-
 }
 
 // Render High Scores
@@ -191,16 +195,16 @@ function showScores () {
 //Logging user's answer ID to the correct answer and changing current index 
 $(answersDiv.children()).on('click', function() {
     var userAnswer = (this.id)
-    console.log(userAnswer)
+    // console.log(userAnswer)
     if (userAnswer === quizArray[currentIndex].correct) {
         score++
     } else {
         timer = timer - 10;
         // score--
     }
-    console.log("Score: " + score)
+    // console.log("Score: " + score)
     currentIndex++;
-    console.log(currentIndex);
+    // console.log(currentIndex);
     if (currentIndex == quizArray.length) {
         endGame();
     } else {
